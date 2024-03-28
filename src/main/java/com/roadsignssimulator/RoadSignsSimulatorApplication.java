040ba8f3b836daa24b9119f8db470a45053afeb5
rbrahinets <rostyslav.brahinets@gmail.com>
2024-03-28 11:43:21 +0200

    Add starter for application

---
+++ src/main/java/com/roadsignssimulator/RoadSignsSimulatorApplication.java
@@ -0,0 +1,11 @@
+package com.roadsignssimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadSignsSimulatorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(com.roadsignssimulator.RoadSignsSimulatorApplication.class, args);
+    }
+}
